03faae0db6386c6ab586167ebf96efe293b13528
Matt Dendura <mjdendura@googlemail.com>
2018-11-29 12:28:44 +0000

    * Added theory for Aged Brie quality increasing with age. * Moved using inside namespace.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,8 +1,7 @@
-using Xunit;
-
 namespace GildedRose.Tests
 {
     using GildedRose.Console;
+    using Xunit;
 
     public class TestAssemblyTests
     {
@@ -11,18 +10,18 @@ namespace GildedRose.Tests
         /// 1 if the sell by date has not yet passed
         /// </summary>
         /// <param name="sellIn">Test data for <see cref="Item.SellIn"/></param>
-        /// <param name="startingQuality">Test data for <see cref="Item.Quality"/></param>
+        /// <param name="quality">Test data for <see cref="Item.Quality"/></param>
         [Theory]
         [InlineData(1, 2)]
         [InlineData(10, 10)]
         [InlineData(int.MaxValue, 5)]
-        public void UpdateQuality_BeforeSellByDate_DecrementsQualityBy1(int sellIn, int startingQuality)
+        public void UpdateQuality_BeforeSellByDate_DecrementsQualityBy1(int sellIn, int quality)
         {
             // Arrange
             var item = new Item
             {
                 Name = "Bag of holding",
-                Quality = startingQuality,
+                Quality = quality,
                 SellIn = sellIn
             };
 
@@ -30,7 +29,7 @@ namespace GildedRose.Tests
             Program.UpdateQuality(item);
 
             // Assert
-            Assert.Equal(startingQuality - 1, item.Quality);
+            Assert.Equal(quality - 1, item.Quality);
         }
 
         /// <summary>
@@ -38,18 +37,18 @@ namespace GildedRose.Tests
         /// 2 if the sell by date has passed
         /// </summary>
         /// <param name="sellIn">Test data for <see cref="Item.SellIn"/></param>
-        /// <param name="startingQuality">Test data for <see cref="Item.Quality"/></param>
+        /// <param name="quality">Test data for <see cref="Item.Quality"/></param>
         [Theory]
         [InlineData(-1, 2)]
         [InlineData(-10, 10)]
         [InlineData(-50, 5)]
-        public void UpdateQuality_AfterSellByDate_DecrementsQualityBy2(int sellIn, int startingQuality)
+        public void UpdateQuality_AfterSellByDate_DecrementsQualityBy2(int sellIn, int quality)
         {
             // Arrange
             var item = new Item
             {
                 Name = "Past its best",
-                Quality = startingQuality,
+                Quality = quality,
                 SellIn = sellIn
             };
 
@@ -57,7 +56,7 @@ namespace GildedRose.Tests
             Program.UpdateQuality(item);
 
             // Assert
-            Assert.Equal(startingQuality - 2, item.Quality);
+            Assert.Equal(quality - 2, item.Quality);
         }
 
         /// <summary>
@@ -82,6 +81,36 @@ namespace GildedRose.Tests
 
             // Assert
             Assert.True(item.Quality >= 0);
+        }
+
+        /// <summary>
+        /// Tests that <see cref="Program.UpdateQuality(Item)"/> increases the quality for Aged Brie as
+        /// the age increases.
+        /// </summary>
+        /// <param name="sellIn"></param>
+        /// <param name="quality"></param>
+        [Theory]
+        [InlineData(2, 5)]
+        [InlineData(0, 10)]
+        [InlineData(-1, 2)]
+        [InlineData(-10, 10)]
+        [InlineData(-50, 5)]
+        public void UpdateQuality_AgedBrie_QualityIncreaseWithAge(int sellIn, int quality)
+        {
+            // Arrange
+            var item = new Item
+            {
+                Name = "Aged Brie",
+                SellIn = sellIn,
+                Quality = quality
+            };
+
+            // Act
+            Program.UpdateQuality(item);
+
+            // Assert
+            Assert.True(item.SellIn < sellIn);
+            Assert.True(item.Quality > quality);
         }
     }
 }

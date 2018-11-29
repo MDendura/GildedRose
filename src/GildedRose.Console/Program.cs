08fb4cc8b9b9c2a519df92ba40b1a7c225b470ae
Matt Dendura <mjdendura@googlemail.com>
2018-11-29 22:46:52 +0000

    * Refactored logic with implementation in separate methods * Added quick-and-dirty test fixture to compare old and new logic

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,10 +1,17 @@
 ﻿namespace GildedRose.Console
 {
+    using System;
     using System.Collections.Generic;
     using GildedRose.Models;
 
+    /// <summary>
+    /// Application to update the inventory of the Gilded Rose.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Collection of items stocked by the Gilded Rose.
+        /// </summary>
         private static IList<Item> Items = new List<Item>
         {
             new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
@@ -15,18 +22,136 @@
             new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
         };
 
-        static void Main(string[] args)
+        /// <summary>
+        /// Application entry point.
+        /// </summary>
+        public static void Main()
         {
-            System.Console.WriteLine("OMGHAI!");
+            Console.WriteLine("OMGHAI!");
 
             foreach (var item in Items)
             {
-                UpdateQuality(item);
+                UpdateItemQuality(item);
             }
 
-            System.Console.ReadKey();
+            Console.ReadKey();
         }
 
+        /// <summary>
+        /// Updates the <see cref="Item.Quality"/> and <see cref="Item.SellIn"/> values of the given item.
+        /// </summary>
+        /// <param name="item">Item to be updated.</param>
+        public static void UpdateItemQuality(Item item)
+        {
+            switch (item.GetItemType())
+            {
+                case ItemType.Legendary:
+                    // Legendary items should not be modified
+                    return;
+
+                case ItemType.Standard:
+                    UpdateStandardItem(item);
+                    break;
+
+                case ItemType.Maturing:
+                    UpdateMaturingItem(item);
+                    break;
+
+                case ItemType.Timed:
+                    UpdateTimedItem(item);
+                    break;
+
+                case ItemType.Conjured:
+                    UpdateConjuredItem(item);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(item), "Item type could not be determined or was invalid.");
+            }
+        }
+
+        /// <summary>
+        /// Updates the <see cref="Item.Quality"/> and <see cref="Item.SellIn"/> values of the given standard item.
+        /// </summary>
+        /// <param name="item">Standard item to be updated.</param>
+        public static void UpdateStandardItem(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0 && item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+
+        /// <summary>
+        /// Updates the <see cref="Item.Quality"/> and <see cref="Item.SellIn"/> values of the given maturing item.
+        /// </summary>
+        /// <param name="item">Maturing item to be updated.</param>
+        public static void UpdateMaturingItem(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+        }
+
+        /// <summary>
+        /// Updates the <see cref="Item.Quality"/> and <see cref="Item.SellIn"/> values of the given timed item.
+        /// </summary>
+        /// <param name="item">Timed item to be updated.</param>
+        public static void UpdateTimedItem(Item item)
+        {
+            if (item.Quality < 49)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.SellIn < 11)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+
+                if (item.SellIn < 6)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+            }
+
+            item.SellIn = item.SellIn - 1;
+        }
+
+        /// <summary>
+        /// Updates the <see cref="Item.Quality"/> and <see cref="Item.SellIn"/> values of the given conjured item.
+        /// </summary>
+        /// <param name="item">Conjured item to be updated.</param>
+        public static void UpdateConjuredItem(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 2;
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0 && item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+        }
+
+        /// <summary>
+        /// Old implementation of logic to update item quality and expiry.
+        /// </summary>
+        /// <param name="item">Item to be updated.</param>
         public static void UpdateQuality(Item item)
         {
             if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
